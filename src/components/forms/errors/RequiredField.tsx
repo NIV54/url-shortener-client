7a397123458ab2088ba101f5a3a9fe4b82dfbfcc
Niv Zelber <nivznivz0@gamil.com>
2021-01-19 00:58:21 +0200

    design login form

--- src/components/forms/errors/RequiredField.tsx
+++ src/components/forms/errors/RequiredField.tsx
@@ -1,3 +1,3 @@
 import React from "react";
 
-export const RequiredField = () => <span>This field is required</span>;
+export const RequiredField = () => <p>This field is required!</p>;
--- src/components/forms/login/Login.tsx
+++ src/components/forms/login/Login.tsx
@@ -7,6 +7,10 @@ import { Login as LoginFields } from "../../../common/types/Login.type";
 import * as messages from "../../../common/user-messages";
 import { RequiredField } from "../errors/RequiredField";
 
+import Icon from "../../../assets/icon.png";
+
+import "./Login.scss";
+
 export const Login = () => {
   const { register, handleSubmit, errors } = useForm<LoginFields>({
     mode: "onSubmit"
@@ -23,29 +27,49 @@ export const Login = () => {
   };
 
   return (
-    <form className="container" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-group">
-        {/* TODO: make this email/username field */}
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          className="form-control"
-          name="email"
-          ref={register({ required: true })}
+    <div className="h-100 content-center text-center">
+      <form className="sign-in-form" onSubmit={handleSubmit(onSubmit)}>
+        <img
+          className="mb-4"
+          src={Icon}
+          alt="Logo should be here..."
+          height="144"
+          width="144"
         />
-      </div>
-      {errors.email && <RequiredField />}
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          className="form-control"
-          name="password"
-          ref={register({ required: true })}
-        />
-      </div>
-      {errors.password && <RequiredField />}
-      <input type="submit" className="btn btn-primary" />
-    </form>
+        <h1 className="h3 mb-2 font-weight-normal">Please sign in</h1>
+        <div className="card-body">
+          {/* TODO: make this email/username field */}
+          <input
+            type="text"
+            className="form-control"
+            name="email"
+            placeholder="Email"
+            required
+            autoFocus
+            autoComplete={"off"}
+            ref={register({ required: true })}
+          />
+          {errors.email && <RequiredField />}
+          <input
+            type="text"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            required
+            autoComplete={"off"}
+            ref={register({ required: true })}
+          />
+          {errors.password && <RequiredField />}
+          <input type="checkbox" className="mb-4 mr-1" name="remember" />
+          <label htmlFor="remember">Remember me</label>
+          <input
+            type="submit"
+            className="btn btn-primary btn-lg btn-block"
+            value="Sign In"
+          />
+          <p className="mt-5 mb-3 text-muted">© {new Date().getFullYear()}</p>
+        </div>
+      </form>
+    </div>
   );
 };
