8c8644ea4f7da12d363a5b30b80c3e7e6ea34c12
Niv Zelber <nivznivz0@gamil.com>
2021-02-01 21:49:04 +0200

    add seo with react helmet

---
+++ src/components/app/SEO/SEO.tsx
@@ -0,0 +1,78 @@
+import { title } from "process";
+import React from "react";
+import { Helmet } from "react-helmet";
+
+import { metadata } from "./metadata";
+
+interface Props {
+  title: string;
+  pathSlug: string;
+  description?: string;
+  keywords?: string[];
+}
+
+export const SEO = ({ title: pageTitle, pathSlug, description, keywords = [] }: Props) => {
+  const { siteTitle, siteDescription, siteUrl, siteKeywords } = metadata;
+  const metaDescription = description || siteDescription;
+  const url = siteUrl + pathSlug;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: "en",
+        dir: "ltr"
+      }}
+      title={pageTitle}
+      titleTemplate={`${siteTitle} | %s`}
+      meta={[
+        {
+          property: `og:title`,
+          content: pageTitle
+        },
+
+        {
+          name: `description`,
+          content: siteDescription
+        },
+        {
+          property: `og:description`,
+          content: metaDescription
+        },
+        {
+          property: "og:url",
+          content: url
+        },
+        {
+          property: `og:type`,
+          content: `website`
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`
+        },
+        {
+          name: `twitter:title`,
+          content: title
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription
+        },
+        {
+          name: "keywords",
+          content: siteKeywords.concat(keywords).join()
+        },
+        {
+          name: "google-site-verification",
+          content: "VOikJ8xB6WuiJewELdwHwfJIs-xM-0RufSUkBVqOWy0"
+        }
+      ]}
+      link={[
+        {
+          rel: "canonical",
+          href: url
+        }
+      ]}
+    />
+  );
+};
---
+++ src/components/app/SEO/metadata.ts
@@ -0,0 +1,6 @@
+export const metadata = {
+  siteTitle: "Nice URL",
+  siteDescription: "Manageable URL Shortener",
+  siteUrl: "https://niceurl.vercel.app",
+  siteKeywords: ["nice", "url", "niceurl", "urlshortener", "bitly"]
+};
